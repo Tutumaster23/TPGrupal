76ca3bf53bc6f7452717b80a06332f558d0a0b25
Tutumaster23 <cristianifrancisco@gmail.com>
2025-11-07 23:05:54 -0300

    Add files via upload

---
+++ include/funciones.h
@@ -0,0 +1,16 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+#include <iostream>
+using namespace std;
+
+void mostrarMenu();
+void jugarUnJugador(int &puntajeTotal, string &nombre);
+void limpiarPantalla();
+void mostrarCreditos();
+void tirarDados(int dados[], int cantidad);
+void mostrarDados(int dados[], int cantidad, int numLanzamiento);
+void contarDados(int dados[], int conteo[]);
+void combinaciones(int conteo[], int &puntajeRonda);
+void puntajemasAlto(int puntajeTotal, string nombre);
+
+#endif // FUNCIONES_H
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "funciones.h"
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand(time(NULL)); ///PARA QUE RAND GENERE NUMEROS ALEATORIOS TODAS LAS PARTIDAS
+
+    int opcion;
+    int puntajeTotal = 0;
+    string nombre = "";
+
+    ///MOSTRAR MENU
+    do{
+        mostrarMenu();
+        cout << "SELECCIONE UNA OPCION: " << endl;
+        cin >> opcion;
+
+        switch(opcion){
+    case 1:
+        //NUEVA PARTIDA(1 JUGADOR)
+        system ("cls");
+        jugarUnJugador(puntajeTotal, nombre);
+        break;
+    case 2:
+        //NUEVA PARTIDA(2 JUGADORES)
+        break;
+    case 3:
+        //VER PUNTUACION MAS ALTA
+        puntajemasAlto(puntajeTotal, nombre);
+        break;
+    case 4:
+        //VER CREDITOS
+        mostrarCreditos();
+        break;
+    case 5:
+        //SALIR
+        cout << "Saliendo del juego..." << endl;
+        break;
+    default:
+        //OPCION NO VALIDA
+        cout << "Opcion no valida, intentalo de nuevo" << endl;
+
+        break;
+        }
+    }while(opcion!=5);
+
+
+
+
+
+
+    return 0;
+}
